17ef581fad53db28e7418ebfe236daea72aa2f51
Alan Rodriguez <alanrodriguezalexandro@gmail.com>
2022-08-10 14:04:51 -0300

    add course converter

--- src/main/java/org/school/management/api/services/ConvertService.java
+++ src/main/java/org/school/management/api/services/ConvertService.java
@@ -1,7 +1,9 @@
 package org.school.management.api.services;
 
+import org.school.management.api.dto.CourseDto;
 import org.school.management.api.dto.StudentDto;
 import org.school.management.api.dto.TeacherDto;
+import org.school.management.api.entities.Course;
 import org.school.management.api.entities.Student;
 import org.school.management.api.entities.Teacher;
 
@@ -14,4 +16,8 @@ public interface ConvertService {
     Teacher convertToEntity(TeacherDto teacherDto);
 
     TeacherDto convertToDto(Teacher teacher);
+
+    Course convertToEntity(CourseDto courseDto);
+
+    CourseDto convertToDto(Course course);
 }
--- src/main/java/org/school/management/api/services/impl/ConvertServiceImpl.java
+++ src/main/java/org/school/management/api/services/impl/ConvertServiceImpl.java
@@ -1,8 +1,10 @@
 package org.school.management.api.services.impl;
 
 import org.modelmapper.ModelMapper;
+import org.school.management.api.dto.CourseDto;
 import org.school.management.api.dto.StudentDto;
 import org.school.management.api.dto.TeacherDto;
+import org.school.management.api.entities.Course;
 import org.school.management.api.entities.Student;
 import org.school.management.api.entities.Teacher;
 import org.school.management.api.services.ConvertService;
@@ -33,5 +35,17 @@ public class ConvertServiceImpl implements ConvertService {
     public TeacherDto convertToDto(Teacher teacher) {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper.map(teacher, TeacherDto.class);
+    }
+
+    @Override
+    public Course convertToEntity(CourseDto courseDto) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(courseDto, Course.class);
+    }
+
+    @Override
+    public CourseDto convertToDto(Course course) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(course, CourseDto.class);
     }
 }
