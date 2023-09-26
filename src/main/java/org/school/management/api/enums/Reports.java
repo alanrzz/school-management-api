44a1f75a3f52ff2a91a0a2671d61775392273bf4
Alan Rodriguez <alanrodriguezalexandro@gmail.com>
2023-09-26 10:52:46 -0300

    test report

--- src/main/java/org/school/management/api/enums/Reports.java
+++ src/main/java/org/school/management/api/enums/Reports.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Reports {
-    CONTENT_DISPOSITION_FORM_DATA("attachment"),REPORTS_FOLDER("reports/"),
+    CONTENT_DISPOSITION_FORM_DATA("attachment"),REPORTS_FOLDER("./reports/"),
     IN_EXTENSION(".jrxml"),OUT_EXTENSION(".pdf"),
     STUDENTS_REPORT("ReporteEstudiantes"),
     TEACHERS_REPORT("ReporteProfesores");
